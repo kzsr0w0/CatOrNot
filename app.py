469282938444447f004e90099f7808ff58d6603d
kzsr0w0 <152373933+kzsr0w0@users.noreply.github.com>
2023-12-11 01:42:35 +0900

    6th commit

--- app.py
+++ app.py
@@ -2,19 +2,16 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.title('Cat or Not')
+st.title('Cat Or Not')
 uploaded_file = st.file_uploader("画像をアップロードしてください", type=['jpeg', 'jpg'])
 
 if uploaded_file is not None:
     # FastAPIに画像を送信
     files = {"file": uploaded_file.getvalue()}
     # ここでURLを文字列として正しく記述
-    response = requests.post("https://catornot.onrender.com", files=files)
+    response = requests.post("https://catornot.onrender.com/make_predictions", files=files)
     #response = requests.post("http://127.0.0.1:8000/make_predictions",files=files)
     # 結果の表示
     if response.status_code == 200:
         result = response.json()
         st.write(f'結果：{result["result"]}')
-    else:
-        # 'st.write' の綴りを修正
-        st.write('エラー')
