9d1894f2f8280dd05b8c0a858d5e15b4b7c13c59
kzsr0w0 <152373933+kzsr0w0@users.noreply.github.com>
2023-12-11 01:51:18 +0900

    7th commit

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import pandas as pd
 import requests
+from io import BytesIO
 
 st.title('Cat Or Not')
 uploaded_file = st.file_uploader("画像をアップロードしてください", type=['jpeg', 'jpg'])
 
 if uploaded_file is not None:
-    # FastAPIに画像を送信
+    # FastAPIサーバーに画像を送信
     files = {"file": uploaded_file.getvalue()}
-    # ここでURLを文字列として正しく記述
     response = requests.post("https://catornot.onrender.com/make_predictions", files=files)
-    #response = requests.post("http://127.0.0.1:8000/make_predictions",files=files)
+           #response = requests.post("http://127.0.0.1:8000/make_predictions",files=files)
+
     # 結果の表示
     if response.status_code == 200:
         result = response.json()
         st.write(f'結果：{result["result"]}')
+    else:
+        st.error(f'エラーが発生しました: {response.status_code}')
+        st.write(response.text)
+
+
+ 
