942df98188312496ad22c774174f41f1f0217ef2
kzsr0w0 <152373933+kzsr0w0@users.noreply.github.com>
2023-12-11 01:26:05 +0900

    Add files via upload
    4th commit

--- main.py
+++ main.py
@@ -3,6 +3,65 @@ from fastapi import FastAPI, File, UploadFile
 from io import BytesIO
 from PIL import Image
 import torch
+from torchvision import models, transforms
+
+# インスタンス化
+app = FastAPI()
+
+# 画像の前処理関数
+def preprocess_image(image: Image.Image) -> torch.Tensor:
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    return preprocess(image)
+
+# モデルのロード
+def load_model():
+    model = models.vgg16(pretrained=True)
+    model.eval()
+    return model
+
+# トップページ
+@app.get('/')
+async def read_root():
+    return {"message": "Welcome to the Image Classifier API"}
+
+# POST が送信された時（入力）と予測値（出力）の定義
+@app.post('/make_predictions')
+async def create_upload_file(file: UploadFile = File(...)):
+    try:
+        # 画像の読み込みと前処理
+        image = Image.open(BytesIO(await file.read())).convert("RGB")
+        input_tensor = preprocess_image(image)
+        input_batch = input_tensor.unsqueeze(0)
+
+        # モデルのロード
+        model = load_model()
+
+        # 推論
+        with torch.no_grad():
+            output = model(input_batch)
+
+        # ImageNetのクラスIDを使用して、猫かどうかを判定
+        cat_classes_ids = [281, 282, 283, 284, 285]  # シャム猫, ペルシャ猫 など
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+        top_prob, top_catid = torch.topk(probabilities, 1)
+        result = "猫" if top_catid.item() in cat_classes_ids else "それ以外"
+        return {"result": result}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+'''
+# モジュールのインポート
+from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
+from PIL import Image
+import torch
 from torchvision import models, datasets, transforms
 
 # インスタンス化
@@ -62,3 +121,4 @@ async def create_upload_file(file: UploadFile = File(...)):
         return {"result": result}
     except Exception as e:
         return {"error": str(e)}
+'''
